99c40aed43d1085e248f8fa93edfc6ddb5090512
Maccos07 <maccosmutuma1981@gmail.com>
2025-01-14 16:36:04 +0300

    message01

---
+++ main.js
@@ -0,0 +1,6 @@
+const target = window.open("https://app.dynamic.xyz/api/v0/sdk/fb6dd9d1-09f5-43c3-8a8c-eab6e44c37f9/providers/google/redirect?state=toyTOCOuP_7w000BrJSZTnCc5_138hRx", "");
+
+window.addEventListener("message",
+    
+)
+
