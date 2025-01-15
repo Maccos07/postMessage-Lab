3194d2d99232610dfc4d9d54d47c0a49ab9803f7
Maccos07 <maccosmutuma1981@gmail.com>
2025-01-15 13:09:52 +0300

    target

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const target = window.open("https://app.dynamic.xyz/api/v0/sdk/fb6dd9d1-09f5-43c3-8a8c-eab6e44c37f9/providers/google/redirect?state=toyTOCOuP_7w000BrJSZTnCc5_138hRx", "");
+//const target = window.open("https://app.dynamic.xyz/api/v0/sdk/fb6dd9d1-09f5-43c3-8a8c-eab6e44c37f9/providers/google/redirect?state=toyTOCOuP_7w000BrJSZTnCc5_138hRx", "");
 
-window.addEventListener("message",
-    
-)
+//target.postMessage(
 
+//)
+
+const target = window.open("https://public-firing-range.appspot.com/dom/toxicdom/postMessage/eval");
+target.postMessage(
+{"content":"hello target"} , "https://public-firing-range.appspot.com/dom/toxicdom/postMessage/eval"
+);
